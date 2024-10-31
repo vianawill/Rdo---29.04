6915c69780cd15061cd8714ce111e2dddb9375a6
gabrieljacobis <gjacobis@hotmail.com>
2024-10-30 22:44:41 -0300

    Campo cpf inserido na tabela users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'cpf' => $this->faker->unique()->numerify('###########'), // 11 dígitos
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,6 +26,28 @@
                         </div>
 
                         <div class="row mb-3">
+                            <label for="cpf" class="col-md-4 col-form-label text-md-end">{{ __('CPF') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="cpf" 
+                                type="text" 
+                                class="form-control @error('cpf') is-invalid @enderror" 
+                                name="cpf" 
+                                value="{{ old('cpf') }}" 
+                                required 
+                                autocomplete="cpf" 
+                                autofocus
+                                oninput="this.value =this.value.replace(/[^0-9]/g, '');">
+
+                                @error('cpf')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
