01a5c94a3df66f5fcc663a8bfc2a0d503f5d9a6a
gabrieljacobis <gjacobis@hotmail.com>
2024-11-09 09:45:27 -0300

    alterei a home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header"><h5>{{ __('Bem vindo') }} {{ Auth::user()->name }}</h5></div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
