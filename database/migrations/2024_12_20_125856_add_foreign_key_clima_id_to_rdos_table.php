507cc9dcefd9c6b353ce8d42c18713219c2dff97
gabrieljacobis <gjacobis@hotmail.com>
2024-12-20 10:34:42 -0300

    Relacionamento da tabela climas com a rdos

--- database/migrations/2024_12_20_125856_add_foreign_key_clima_id_to_rdos_table.php
+++ database/migrations/2024_12_20_125856_add_foreign_key_clima_id_to_rdos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForeignKeysToRdosTable extends Migration
+class AddForeignKeyClimaIdToRdosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,10 +14,6 @@ class AddForeignKeysToRdosTable extends Migration
     public function up()
     {
         Schema::table('rdos', function (Blueprint $table) {
-            // adicionar chave estrangeira para condicao_id
-            // $table->foreign('condicao_id')->references('id')->on('condicao_areas')->onDelete('cascade');
-            // adicionar chave estrangeira para acidente_id
-            // $table->foreign('acidente_id')->references('id')->on('acidentes')->onDelete('cascade');
             // adicionar chave estrangeira para clima_id
             $table->foreign('clima_id')->references('id')->on('climas')->onDelete('cascade');
         });
@@ -31,10 +27,6 @@ class AddForeignKeysToRdosTable extends Migration
     public function down()
     {
         Schema::table('rdos', function (Blueprint $table) {
-            // remover chave estrangeira de condicao_id
-            $table->dropForeign(['condicao_id']);
-            // remover chave estrangeira de acidente_id
-            $table->dropForeign(['acidente_id']);
             // remover chave estrangeira de clima_id
             $table->dropForeign(['clima_id']);
         });
